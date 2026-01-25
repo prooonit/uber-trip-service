75c66b36bc9a15d6d061b8d68d03897c259f872b
prooonit <109405480+prooonit@users.noreply.github.com>
2026-01-25 15:43:51 +0530

    update the driver service class

--- src/modules/driver/driver.service.js
+++ src/modules/driver/driver.service.js
@@ -9,31 +9,36 @@ exports.storeLocation = async (driverId, lat, lng) => {
     member: driverId
   });
 
-  await redis.set(
-  `driver:heartbeat:${driverId}`,1,{ EX: 30 } );
+  await redisClient.set(
+    `driver:heartbeat:${driverId}`,
+    "1",
+    { EX: 30 }
+  );
 };
 
 exports.removeDriver = async (driverId) => {
-  await  redis.del(`driver:heartbeat:${driverId}`);
+  await redisClient.zRem(GEO_KEY, driverId);
+  await redisClient.del(`driver:heartbeat:${driverId}`);
 };
 
 exports.findNearbyDrivers = async (lat, lng, radius) => {
-  const nearbyDrivers = await redis.geoRadius(
-  GEO_KEY,lng,lat,radius,'km'
+ 
+  const nearbyDrivers = await redisClient.geoSearch(
+    GEO_KEY,
+    { longitude: lng, latitude: lat },
+    { radius: radius, unit: "km" }
   );
-  if(!nearbyDrivers.length)return [];
-  
-  const pipeline = redis.pipeline();
-  nearbyDrivers.forEach(driverId => {
-    pipeline.exists(`driver:heartbeat:${driverId}`);
-  });
-  const results = await pipeline.exec();
-  const activeDrivers = [];
-  for(let i = 0; i < nearbyDrivers.length; i++){
-    if(results[i][1] === 1){
-      activeDrivers.push(nearbyDrivers[i]);
-    }
-  }
 
-  return activeDrivers;
-}
+  if (!nearbyDrivers.length) return [];
+
+// we are using redis pipeline to check heartbeats
+  const multi = redisClient.multi();
+
+  nearbyDrivers.forEach(driverId => {
+    multi.exists(`driver:heartbeat:${driverId}`);
+  });
+
+  const results = await multi.exec();
+
+  return nearbyDrivers.filter((_, i) => results[i][1] === 1);
+};
